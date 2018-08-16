bd24eb01b7488e6dc160abd1ff5918f52d10840f
jiaolong.liu <715765300@qq.com>
2018-08-16 17:30:39 +0800

    *修改run.sh

--- run.sh
+++ run.sh
@@ -7,19 +7,19 @@
 
 case $1 in 
 	start)
-		nohup ./GmToos 2>&1 >> info.log 2>&1 /dev/null &
+		nohup ./GmTools 2>&1 >> info.log 2>&1 /dev/null &
 		echo "服务已启动..."
 		sleep 1
 	;;
 	stop)
-		killall GmToos
+		killall GmTools
 		echo "服务已停止..."
 		sleep 1
 	;;
 	restart)
-		killall GmToos
+		killall GmTools
 		sleep 1
-		nohup ./GmToos 2>&1 >> info.log 2>&1 /dev/null &
+		nohup ./GmTools 2>&1 >> info.log 2>&1 /dev/null &
 		echo "服务已重启..."
 		sleep 1
 	;;
